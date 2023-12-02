e020fb0f8a6d0480ae211fae80cd0c0734eda013
Ruthikr <147206849+Ruthikr@users.noreply.github.com>
2023-12-02 13:08:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-st.title("**Welcome**")
-st.write("Enter the details")
+st.title("Enter the transaction details")
 st.write("The model will predict whether they are fraud or not")
 pipe=pickle.load(open("bank_fraud_pipeline.pkl","rb"))
 
